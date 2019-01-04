606ce6ffbd378250f2a5459118afc3824539bcef
Nathan Dunn <ndunn@me.com>
2019-01-03 21:24:23 -0800

    cleaned out the image for upload

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ cd /apollo/ && \
     cp /apollo/target/apollo*.war /tmp/apollo.war && \
 	# here we save the tools directory
 	# So we can remove ~1.6 GB of cruft from the image. Ignore errors because cannot remove parent dir /apollo/
-#    rm -rf /apollo/ || true && \
+    rm -rf /apollo/ || true && \
 	# Before moving back into a standardized location (that we have write access to)
 	mv /tmp/apollo.war /apollo/apollo.war
